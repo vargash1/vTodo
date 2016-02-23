397771e6869686d23dacf4f45e4cd7ffc3b2e796
Hector Vargas <vargash1@wit.edu>
2016-02-23 10:15:48 -0500

    Improvements to webpage Notes using css Renaming project to vTask soon

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-
 var express = require('express');
 var app = express();
 
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var favicon = require('serve-favicon');
+app.use('/',express.static('public'));
 
-app.use('/static',express.static('public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
-app.get('/', function (req, res) {
-      res.send('Hello World!');
-});
 
 app.listen(3000, function () {
       console.log('Example app listening on port 3000!');
