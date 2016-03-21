fce9cdcff13095a89193804190f412c2139910c6
Hector Vargas <vargash1@wit.edu>
2016-03-20 21:32:56 -0400

    Small changes to app.js before moving on.

--- vtodo.js
+++ vtodo.js
@@ -14,29 +14,29 @@ var LocalStrategy = require('passport-local').Strategy;
 var app = express();
 var session = require('express-session');
 var conString = "pg://vargash1:guest@localhost:5432/vtodo_db";
-var dbclient = new pg.Client(conString);
 
-dbclient.connect();
 passport.use(new LocalStrategy({
     usernameField: 'username',
     passwordField: 'password'
 },
 function(username,password,done){
-    console.log("[INFO] Connected to Database: psql");
-    dbclient.query('SELECT * FROM users WHERE username = $1', [username], function(err, result) {
-    if (err){
-        console.log("[INFO] Database Query Error");
-        return done(err);
-    }
-    if (result.rows.length > 0){
-        var matched = bcrypt.compareSync(password, result.rows[0].password);
-        if (matched) {
-            console.log("[INFO] Successful Login");
-            return done(null, result.rows[0]);
+    pg.connect(conString, function(err, client, next){
+        if (err){
+            console.error("[INFO] Unable To Connect To Database");
         }
-    }
-    console.log("[INFO] Invalid Username or Password");
-    return done(null, false);
+        console.log("[INFO] Connected to Database");
+        client.query('SELECT * FROM users WHERE username = $1', [username], function(err, result) {
+            next();
+            if (result.rows.length > 0){
+                var matched = bcrypt.compareSync(password, result.rows[0].password);
+                if (matched) {
+                    console.log("[INFO] Successful Login");
+                    return done(null, result.rows[0]);
+                }
+            }
+            console.log("[INFO] Invalid Username or Password");
+            return done(null, false);
+        })
     });
 }));
 
@@ -47,12 +47,15 @@ passport.serializeUser(function(user, done) {
 
 // Get user information out of session
 passport.deserializeUser(function(id, done) {
-      dbclient.query('SELECT id, username FROM users WHERE id = $1', [id], function(err, result) {
-      // Return the user
-      if (result) {
-        return done(null, result.rows[0]);
-      }
-      return done(null, false);
+    pg.connect(conString,function(err,client,next){
+        client.query('SELECT id, username FROM users WHERE id = $1', [id], function(err, result) {
+            next()
+            // Return the user
+            if (result) {
+                return done(null, result.rows[0]);
+            }
+            return done(null, false);
+        });
     });
 });
 
@@ -60,8 +63,7 @@ passport.deserializeUser(function(id, done) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public','images','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -114,14 +116,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-//
-// dbclient.query("CREATE TABLE IF NOT EXISTS userstest(firstname varchar(64), lastname varchar(64))");
-// dbclient.query("INSERT INTO userstest(firstname,lastname) values ($1, $2)", ['kek','top']);
-// var query = dbclient.query("SELECT firstname, lastname FROM userstest ORDER BY lastname, firstname");
-// query.on("row", function (row, result) {
-//     result.addRow(row);
-// });
-// query.on("end", function (result) {
-//     console.log(JSON.stringify(result.rows, null, "    "));
-//     dbclient.end();
-// });
